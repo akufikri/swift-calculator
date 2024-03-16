c7b5ac44b6bc6bfe03e0623624636ad6f73eccc1
Fikri Nurhakim <108182945+akufikri@users.noreply.github.com>
2024-03-16 07:50:48 +0700

    Initial Commit

---
+++ iCalculator/iCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCalculatorApp.swift
+//  iCalculator
+//
+//  Created by Fikri Nurhakim on 16/03/24.
+//
+
+import SwiftUI
+
+@main
+struct iCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
